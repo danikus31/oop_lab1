3c2738a7249b06da8bcc7fedcac1a7207db78f7a
danikus31 <58368856+danikus31@users.noreply.github.com>
2024-09-17 13:06:22 +0300

    be patent

--- lab1/organization.cpp
+++ lab1/organization.cpp
@@ -7,7 +7,7 @@ void organization::showBildings()
 {
     system("cls");
     if (universitys.size()) {
-        cout << "Total universitys: " << universitys.size() << "\n";
+        cout << "Number of universitys: " << universitys.size() << "\n";
         VariadicTable<short, string, short, short, float> vt({ "ID", "Name", "founders date", "students", "avrage mark"}, 10);
         for (size_t i = 0; i < universitys.size(); ++i) {
             universitys[i].numberOfStudents = 0;
